6f02a7e24e18f1d6e644c7426b6f397811d26773
lingbeijing <lingbeijing@miniwan.com>
2020-09-01 18:51:37 +0800

    retrofit 接入

--- app/src/main/java/com/littleox/common/app/network/ApiService.java
+++ app/src/main/java/com/littleox/common/app/network/ApiService.java
@@ -1,9 +1,16 @@
 package com.littleox.common.app.network;
 
+import com.littleox.common.app.bean.GithubUser;
+
+import io.reactivex.Flowable;
+import retrofit2.http.GET;
+
 /**
  * Created by lingbeijing on 2020/9/1.
  */
 public interface ApiService {
-    
-    
+
+    @GET("/user/octokit")
+    Flowable<GithubUser> getUserOctokit();
+
 }
--- app/src/main/java/com/littleox/common/app/network/WebApi.java
+++ app/src/main/java/com/littleox/common/app/network/WebApi.java
@@ -1,8 +1,59 @@
 package com.littleox.common.app.network;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by lingbeijing on 2020/9/1.
  */
 public class WebApi {
-    
+
+    //读超时长，单位：毫秒
+    public static final int READ_TIME_OUT = 30000;
+    //连接时长，单位：毫秒
+    public static final int CONNECT_TIME_OUT = 30000;
+    private static volatile WebApi mInstance;
+
+    private Retrofit mRetrofit;
+    private final String BASE_URL = "https://api.github.com/";
+
+
+    private WebApi() {
+        init();
+    }
+
+    public static WebApi getInstance() {
+        if (mInstance == null) {
+            synchronized (WebApi.class) {
+                if (mInstance == null) {
+                    mInstance = new WebApi();
+                }
+            }
+        }
+        return mInstance;
+    }
+
+    private void init(){
+        if(mRetrofit == null){
+            OkHttpClient.Builder builder= new OkHttpClient.Builder()
+                    .readTimeout(READ_TIME_OUT, TimeUnit.MILLISECONDS)
+                    .connectTimeout(CONNECT_TIME_OUT, TimeUnit.MILLISECONDS)
+                    .addInterceptor(new HttpLoggingInterceptor());
+            mRetrofit = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(builder.build())
+                    .baseUrl(BASE_URL)
+                    .build();
+        }
+    }
+
+    public ApiService getService() {
+        return mRetrofit.create(ApiService.class);
+    }
 }
