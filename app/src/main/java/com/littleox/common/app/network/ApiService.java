4845e356321d1ce191a50a868371e7f2bcb3e731
lingbeijing <lingbeijing@miniwan.com>
2020-09-01 11:40:49 +0800

    app 中引入 commonlib 模块

---
+++ app/src/main/java/com/littleox/common/app/network/ApiService.java
@@ -0,0 +1,9 @@
+package com.littleox.common.app.network;
+
+/**
+ * Created by lingbeijing on 2020/9/1.
+ */
+public interface ApiService {
+    
+    
+}
---
+++ app/src/main/java/com/littleox/common/app/network/WebApi.java
@@ -0,0 +1,8 @@
+package com.littleox.common.app.network;
+
+/**
+ * Created by lingbeijing on 2020/9/1.
+ */
+public class WebApi {
+    
+}
